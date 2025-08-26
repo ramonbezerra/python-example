0d3f4c1a3351575a073fe33c8379a313b6ffa02e
ramonbezerra <ramon.bezerra@outlook.com>
2025-08-26 19:22:11 -0300

    make it better

--- example/wsgi.py
+++ example/wsgi.py
@@ -14,3 +14,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
 
 application = get_wsgi_application()
+
+# import os
+# import sys
+
+# # add your project directory to the sys.path
+# project_home = '/home/rb0nobrega/mysite'
+# if project_home not in sys.path:
+#     sys.path.insert(0, project_home)
+
+# # set environment variable to tell django where your settings.py is
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+
+# # serve django via WSGI
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
